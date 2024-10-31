92425a49261799bd61ed82471ce2959423ffe1d0
Carlos <carlos.mendozav81@gmail.com>
2024-10-31 14:19:27 -0300

    Se genera clase ConvertirMoneda

--- src/ConvertirMoneda.java
+++ src/ConvertirMoneda.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class ConvertirMoneda {
+
+    public static void convertir(String monedaBase, String monedaTarget, ConsultarMoneda consulta, Scanner lectura){
+        double cantidad;
+        double cantidadConvertida;
+
+        Monedas monedas = consulta.buscarMoneda(monedaBase, monedaTarget);
+        System.out.println("Ingresa el valor que desea covertir");
+    }
 }
